afd544f2daac7fcd55dfcf39eca0bcd80dab8e2e
jtreeves <jackson.t.reeves@gmail.com>
2022-11-12 18:04:05 -0500

    [src] Create basic function to add two numbers as proof of concept

---
+++ src/movie_quotes_extractor.cpp
@@ -0,0 +1,5 @@
+#include "movie_quotes_extractor.h"
+
+int MovieQuotesExtractor::add_two_numbers(int first_number, int second_number) {
+    return first_number + second_number;
+};
---
+++ src/movie_quotes_extractor.h
@@ -0,0 +1,4 @@
+class MovieQuotesExtractor {
+    public:
+        int add_two_numbers(int first_number, int second_number);
+};
